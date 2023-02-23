410adef6483e4afe1d68e5e25368eeb109186b34
Édouard Lopez <contact@edouard-lopez.com>
2023-02-23 23:03:35 +0100

    refactor: follow internal wording (directory) and pass parameter as argument to function

--- functions/_pure_parse_directory.fish
+++ functions/_pure_parse_directory.fish
@@ -2,13 +2,13 @@ function _pure_parse_directory \
     --description "Replace '$HOME' with '~'" \
     --argument-names max_path_length
 
-    set --local folder (fish_prompt_pwd_dir_length=$pure_shorten_prompt_current_directory_length prompt_pwd)
+    set --local directory (prompt_pwd --dir-length=$pure_shorten_prompt_current_directory_length)
 
     if test -n "$max_path_length";
-        if test (string length $folder) -gt $max_path_length;
+        if test (string length $directory) -gt $max_path_length;
             # If path exceeds maximum symbol limit, force fish path formating function to use 1 character
-            set folder (fish_prompt_pwd_dir_length=1 prompt_pwd)
+            set directory (prompt_pwd --dir-length=1)
         end
     end
-    echo $folder
+    echo $directory
 end
